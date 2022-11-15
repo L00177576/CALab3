74fc340e7851b0680ea6127e2ff11d3e1502b40c
Anuradha Goli <L00177576@atu.ie>
2022-11-15 23:20:40 +0000

    New files added

---
+++ MavenJavaproject/src/main/java/TestClass.java
@@ -0,0 +1,15 @@
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//this is a test class
+		//nothing happening here!
+		System.out.println("HEllo");
+		System.out.println("GitHub Action!");
+		System.out.println("GitHub Action!");
+		System.out.println("GitHub ActionDemo!");		
+		System.out.println("GitHub ActionDemo!");
+	}
+
+}
